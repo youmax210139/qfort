95c0dfe8f972a542a08bfaeb41fbb53391178b0f
charlie <charlieyuan@dc.com>
2019-10-24 21:39:35 +0800

    fix : guest export

---
+++ app/Exports/Guest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Exports;
+
+class Guest extends BaseExport
+{
+    protected $model = 'App\Models\Guest';
+
+    public function headings(): array
+    {
+        return [
+            "id",
+            "name",
+            "email",
+            "jobTitle",
+            "organization",
+            "area",
+            "country",
+            // "password",
+            "telephone",
+            "created_at",
+            "updated_at",
+        ];
+    }
+
+
+}
