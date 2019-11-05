e590c11f0b6617705a2fe67958e2f633f2f2a777
charlie <charlieyuan@dc.com>
2019-11-05 21:51:42 +0800

    fix

--- app/Http/Controllers/Voyager/VoyagerUserController.php
+++ app/Http/Controllers/Voyager/VoyagerUserController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers\Voyager;
 
+use Illuminate\Http\Request;
 use TCG\Voyager\Http\Controllers\VoyagerUserController as BaseVoyagerUserController;
 
 class VoyagerUserController extends BaseVoyagerUserController
 {
-    //
+    // POST BR(E)AD
+    public function update(Request $request, $id)
+    {
+        if (app('VoyagerAuth')->user()->getKey() == $id) {
+            $request->merge([
+                'role_id'                              => app('VoyagerAuth')->user()->role_id,
+                'user_belongsto_role_relationship'     => app('VoyagerAuth')->user()->role_id,
+                'user_belongstomany_role_relationship' => app('VoyagerAuth')->user()->roles->pluck('id')->toArray(),
+            ]);
+        }
+
+        parent::update($request, $id);
+        return redirect()
+        ->route("voyager.users.edit", $id)
+        ->with([
+            'message'    => __('voyager::generic.successfully_updated')." User",
+            'alert-type' => 'success',
+        ]);
+    }
 }
